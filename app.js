583c4fb7791216b847105b9b3bef6b5da004eb3d
Alexey Perov <alvperov@gmail.com>
2021-11-30 23:57:27 +0300

    Simple parsing logic

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 require('dotenv').config();
 
-const sls = require("serverless-http");
+const sls = require('serverless-http');
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const multer = require('multer');
@@ -23,42 +23,97 @@ app.use(express.urlencoded({extended: true}));
 app.use(upload.array());
 app.use(express.static('public'));
 
-app.get("/unfurl", async (req, res) => {
-    const link = req.query.link;
+app.get('/unfurl', async (req, res) => {
 
-    if (link.contains(process.env.MONDAY_DOMAIN)) {
-        let result = await unfurlMondayLink(link);
-        res.status(200).send(result);
-    } else if (link.contains(process.env.LOGKEEPER_DOMAIN)) {
-        let result = await unfurlLogKeeperLink(link);
-        res.status(200).send(result);
-    } else {
+    try {
+        const link = req.query.link;
+        if (link.includes(process.env.MONDAY_DOMAIN)) {
+            let result = await unfurlMondayLink(link);
+            res.status(200).send(result);
+        } else if (link.includes(process.env.LOGKEEPER_DOMAIN)) {
+            let result = await unfurlLogKeeperLink(link);
+            res.status(200).send(result);
+        } else {
+            res.status(501).send();
+        }
+    } catch (e) {
+        console.log(e);
         res.status(501).send();
     }
 });
 
-app.get("/monday/unfurl", async (req, res) => {
+app.get('/monday/unfurl', async (req, res) => {
     const link = req.query.link;
     let result = await unfurlMondayLink(link);
     res.status(200).send(result);
 });
 
-app.get("/logkeeper/unfurl", async (req, res) => {
+app.get('/logkeeper/unfurl', async (req, res) => {
     const link = req.query.link;
     let result = await unfurlLogKeeperLink(link);
     res.status(200).send(result);
 });
 
+// Parses strings like:
+// https://domain.monday.com/boards/1802709835/views/37620753/pulses/1957128768
+// https://domain.monday.com/boards/1802709835/views/37620753/pulses/1959492575?userId=10386480
 async function unfurlMondayLink(link) {
+    let board;
+    let pulse;
 
+    const boardStr = 'boards';
+    const boardPos = link.indexOf(boardStr);
+
+    if (boardPos === -1) {
+        throw `Unable to find ${boardStr}`;
+    }
+
+    const linkPart = link.substring(boardPos + boardStr.length + 1);
+    const boardEndPos = linkPart.indexOf('/');
+
+    if (boardEndPos !== -1) {
+        board = linkPart.substring(0, boardEndPos);
+    } else {
+        board = linkPart;
+    }
+
+    const pulsesStr = 'pulses';
+
+    const pulsePos = linkPart.indexOf(pulsesStr);
+
+    if (pulsePos !== -1) {
+        const pulseLinkPart = linkPart.substring(pulsePos + pulsesStr.length + 1);
+        const pulseBorderPos = pulseLinkPart.indexOf('?');
+
+        if (pulseBorderPos !== -1) {
+            pulse = pulseLinkPart.substring(0, pulseBorderPos);
+        } else {
+            pulse = pulseLinkPart;
+        }
+    }
+
+    return `${board} -> ${pulse}`;
 }
 
+// Parses strings like: https://domain/#/details?id=zEVVt0ltW65vdoppV0Eg
 async function unfurlLogKeeperLink(link) {
+    const idStr = '?id=';
+    const pos = link.indexOf(idStr);
+    if (pos === -1) {
+        throw `Invalid LogKeeper string. Cannot find ${idStr} position`;
+    }
 
+    const id = link.substring(pos + idStr.length);
+
+    if ( id == null || id.length === 0) {
+        throw 'Unable to find logKeeper id';
+    }
+
+    return id;
 }
 
 async function fetchMondayQuery(query) {
-    let response = await fetch("https://api.monday.com/v2", {
+    let response = await fetch('https://api.monday.com/v2', {
         method: 'post',
         headers: {
             'Content-Type': 'application/json',
